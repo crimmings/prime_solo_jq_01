bce1090d8e52abf9a4fd767ab1b6771767d7ecd3
John Crimmings <john.crimmings@gmail.com>
2015-11-15 00:09:07 -0600

    jquery updates

--- weekend.js
+++ weekend.js
@@ -5,13 +5,27 @@ $(document).ready(function() {
   $('form').on('submit', function(event) {
 
 
-  var  data = $(this).serializeArray(); // turns form data into object
-    console.log(data);
 
+
+  var  data = $(this).serializeArray(); // turns form data into object
+  /*  len = data.length,
+    dataObj = {};
+    $(data).each(function(i, field){
+      dataObj[field.name] = field.value;
+
+    });
+
+for (i=0; i<len; i++) {
+  dataObj[data[i].name] = data[i].value;
+console.log(data[i].value);
+};
+*/
 
   data.forEach(function(elem) { //goes through content in object and assigns values
       var $div = $('<div>');
       var $p = $('<p>');
+      var $li = $('<li>');
+      var $ul = $('<ul>');
       var $btn = $('<button>You Are Fired</button>');
 
 
@@ -19,46 +33,66 @@ $(document).ready(function() {
       switch (elem.name) {
         case "firstname":
           elem.name = "First Name";
-          $p.text(elem.name + ": " + elem.value);
+          $li.text(elem.name + ":");
+          $p.text(elem.value);
+          $('#content').append($li);
           $('#content').append($p);
           break;
         case "lastname":
           elem.name = "Last Name";
-          $p.text(elem.name + ": " + elem.value);
+          $li.text(elem.name + ":");
+          $p.text(elem.value);
+          $('#content').append($li);
           $('#content').append($p);
           break;
         case "employeenumber":
           elem.name = "Employee Number";
-          $p.text(elem.name + ": " + elem.value);
+          $li.text(elem.name + ":");
+          $p.text(elem.value);
+          $('#content').append($li);
           $('#content').append($p);
           break;
         case "title":
           elem.name = "Title";
-          $p.text((elem.name) + ": " + elem.value);
+          $li.text(elem.name + ":");
+          $p.text(elem.value);
+          $('#content').append($li);
           $('#content').append($p);
           break;
         case "lastreviewscore":
           elem.name = "Last Review Score";
           if (elem.value === "5"){
-            $p.text(elem.name + ": " + elem.value).css('color', 'red');
+            $li.text(elem.name + ": ");
+            $p.text(elem.value).css('color', 'red');
+            $('#content').append($li);
               $('#content').append($p);
           } else if (elem.value === "4"){
-            $p.text(elem.name + ": " + elem.value).css('color', 'blue');
-              $('#content').append($p);
+            $li.text(elem.name + ": ");
+            $p.text(elem.value).css('color', 'blue');
+            $('#content').append($li);
+            $('#content').append($p);
           } else if (elem.value === "3"){
-            $p.text(elem.name + ": " + elem.value).css('color', 'green');
-              $('#content').append($p);
+            $li.text(elem.name + ": ");
+            $p.text(elem.value).css('color', 'green');
+            $('#content').append($li);
+            $('#content').append($p);
           } else if (elem.value === "2"){
-            $p.text(elem.name + ": " + elem.value).css('color', 'orange');
-              $('#content').append($p);
+            $li.text(elem.name + ": ");
+            $p.text(elem.value).css('color', 'orange');
+            $('#content').append($li);
+            $('#content').append($p);
           } else {
-            $p.text(elem.name + ": " + elem.value).css('color', 'yellow');
-              $('#content').append($p);
+            $li.text(elem.name + ": ");
+            $p.text(elem.value).css('color', 'yellow');
+            $('#content').append($li);
+            $('#content').append($p);
             }
           break;
         case "salary":
           elem.name = "Salary";
-          $p.text(elem.name + ": " + "$" + elem.value);
+          $li.text(elem.name + ": ");
+          $p.text("$" + elem.value);
+          $('#content').append($li);
           $('#content').append($p).append($btn);
           break;
 
@@ -70,5 +104,11 @@ event.preventDefault();
 
 
 })
-})
+
+
+
+
+
+		});
+
 });
